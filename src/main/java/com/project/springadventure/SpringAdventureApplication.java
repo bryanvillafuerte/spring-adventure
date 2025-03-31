3a52a16c09b7e44ca0a8aedbec52b61210ae2db9
Bryan <bryan@inkeria.no>
2025-03-31 18:28:18 +0200

    initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/project/springadventure/SpringAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.project.springadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdventureApplication.class, args);
+    }
+
+}
